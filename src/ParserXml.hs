b17aae7669d195af42f8f191bcd504d5afb608c6
landryarki <landry.gigant@epitech.eu>
2024-04-25 22:23:52 +0200

    :sparkles: XML parser

--- src/ParserXml.hs
+++ src/ParserXml.hs
@@ -13,6 +13,8 @@ import ParserData
 import DataStruct
 import Control.Applicative (Alternative(..))
 
+------------
+
 parseWhiteSpace :: Parser String
 parseWhiteSpace = parseMany (parseAnyChar " \n\t")
 
@@ -51,7 +53,7 @@ parseHeaderAuthorAndDate title "author" = do
     else do
         parseWhiteSpace
         date <- parseMany (parseNotChar '<')
-        parseFlag "/date"
+        parseFlag "/date" *> parseWhiteSpace *> parseFlag "/header"
         return (Header title (Just author) (Just date)))
 parseHeaderAuthorAndDate title "date" = do
     date <- parseMany (parseNotChar '<')
@@ -61,7 +63,7 @@ parseHeaderAuthorAndDate title "date" = do
     else do
         parseWhiteSpace
         author <- parseMany (parseNotChar '<')
-        parseFlag "/author"
+        parseFlag "/author" *> parseWhiteSpace *> parseFlag "/header"
         return (Header title (Just author) (Just date)))
 
 parseHeader :: Parser Header
@@ -75,3 +77,104 @@ parseHeader = do
     else
       parseWhiteSpace >>
       parseHeaderAuthorAndDate title arg
+
+----- Body -----
+
+parseText :: Parser Element
+parseText = do
+    text <- parseSome (parseNotChar '<')
+    return (Text text)
+
+parseBold :: Parser Element
+parseBold = do
+    parseFlag "bold"
+    elem <- parseElement
+    parseFlag "/bold"
+    return (Bold elem)
+
+parseItalic :: Parser Element
+parseItalic = do
+    parseFlag "italic"
+    elem <- parseElement
+    parseFlag "/italic"
+    return (Italic elem)
+
+parseCode :: Parser Element
+parseCode = do
+    parseFlag "code"
+    elem <- parseElement
+    parseFlag "/code"
+    return (Code elem)
+
+parseList :: Parser Element
+parseList = do
+    parseFlag "list"
+    elems <- parseMany parseElement
+    parseFlag "/list"
+    return (List elems)
+
+parseParagraph :: Parser Element
+parseParagraph = do
+    parseFlag "paragraph"
+    elems <- parseMany parseElement
+    parseFlag "/paragraph"
+    return (Paragraph elems)
+
+parseCodeBlock :: Parser Element
+parseCodeBlock = do
+    parseFlag "codeblock"
+    elems <- parseMany parseElement
+    parseFlag "/codeblock"
+    return (CodeBlock elems)
+
+parseLink :: Parser Element
+parseLink = do
+    (_, url) <- parseFlagWithAttr "link" "url"
+    elems <- parseMany parseElement
+    parseFlag "/link"
+    return (Link (LinkType url elems))
+
+parseImage :: Parser Element
+parseImage = do
+    (_, url) <- parseFlagWithAttr "image" "url"
+    alt <- parseMany parseElement
+    parseFlag "/image"
+    return (Image (ImageType url alt))
+
+parseSection :: Parser Element
+parseSection = do
+    (_, title) <- parseFlagWithAttr "section" "title"
+    elems <- parseMany parseElement
+    parseFlag "/section"
+    return (Section (SectionType title elems))
+    
+---
+
+parseElement :: Parser Element
+parseElement = do
+    parseWhiteSpace
+    elem <- parseBold <|> parseItalic <|> parseCode <|> parseList <|>
+        parseParagraph <|> parseCodeBlock <|> parseLink <|> parseImage <|>
+        parseSection <|> parseText
+    parseWhiteSpace
+    return elem
+
+parseBody :: Parser Body
+parseBody = do
+    parseFlag "body"
+    elems <- parseMany parseElement
+    parseFlag "/body"
+    return (Body elems)
+    
+------------
+
+parseDocument :: Parser Document
+parseDocument = do
+    parseFlag "document"
+    parseWhiteSpace
+    header <- parseHeader
+    parseWhiteSpace
+    body <- parseBody
+    parseWhiteSpace
+    parseFlag "/document"
+    return (Document header body)
