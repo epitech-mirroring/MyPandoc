d984b991a402e16d196f3c84b283cf3e85dcc10b
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-26 16:41:15 +0200

    :rotating_light: Fix warnings

--- src/ParserData.hs
+++ src/ParserData.hs
@@ -10,9 +10,9 @@ module ParserData (
     ) where
 
 
-import Data.Char
+import Data.Char ()
 import Control.Applicative (Alternative(..))
-import Control.Monad
+import Control.Monad ()
 
 
 data Parser a = Parser {
@@ -21,33 +21,33 @@ data Parser a = Parser {
 
 instance Functor Parser where
     fmap fct parser = Parser $ \str -> case runParser parser str of
-        Just (a, str) -> Just (fct a, str)
+        Just (a, s) -> Just (fct a, s)
         Nothing -> Nothing
 
 instance Applicative Parser where
     pure a = Parser $ \str -> Just (a, str)
     parser1 <*> parser2 = Parser $ \str -> case runParser parser1 str of
-        Just (fct, str) -> case runParser parser2 str of
-            Just (a, str) -> Just (fct a, str)
+        Just (fct, s) -> case runParser parser2 s of
+            Just (a, ss) -> Just (fct a, ss)
             Nothing -> Nothing
         Nothing -> Nothing
     parser1 *> parser2 = Parser $ \str -> case runParser parser1 str of
-        Just (_, str) -> runParser parser2 str
+        Just (_, s) -> runParser parser2 s
         Nothing -> Nothing
     parser1 <* parser2 = Parser $ \str -> case runParser parser1 str of
-        Just (a, str) -> case runParser parser2 str of
-            Just (_, str) -> Just (a, str)
+        Just (a, s) -> case runParser parser2 s of
+            Just (_, ss) -> Just (a, ss)
             Nothing -> Nothing
         Nothing -> Nothing
 
 instance Alternative Parser where
     empty = Parser $ \_ -> Nothing
     parser1 <|> parser2 = Parser $ \str -> case runParser parser1 str of
-        Just (a, str) -> Just (a, str)
+        Just (a, s) -> Just (a, s)
         Nothing -> runParser parser2 str
 
 instance Monad Parser where
     return = pure
     parser >>= fct = Parser $ \str -> case runParser parser str of
-        Just (a, str) -> runParser (fct a) str
+        Just (a, s) -> runParser (fct a) s
         Nothing -> Nothing
