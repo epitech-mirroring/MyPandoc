8075d6fb012bcd8f841747ecae7aaf7e47914f1e
landryarki <landry.gigant@epitech.eu>
2024-04-27 18:09:16 +0200

    :bug: remove redundant whitespace

--- src/XML/ParserXML.hs
+++ src/XML/ParserXML.hs
@@ -39,6 +39,7 @@ parseXMLDocument = do
     bodyContent <- parseXMLBody
     _ <- parseWhiteSpace
     _ <- parseXMLFlag "/document"
+    parseWhiteSpace
     return (Document headerContent bodyContent)
 
 getXMLDocument :: String -> Maybe Document
