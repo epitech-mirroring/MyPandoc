1b2c6f74d51179895f6bb9ac213e0f1fbe981712
6im0n <simon.gl3135@orange.fr>
2024-04-27 16:29:38 +0200

    ✨ Add Core module for handling document creation and writing

---
+++ src/Core.hs
@@ -0,0 +1,29 @@
+{-
+-- EPITECH PROJECT, 2024
+-- My Pandoc
+-- File description:
+-- Core
+-}
+
+module Core (
+    writeTheDoc
+) where
+
+import HandleArgs (Options(..), App(..))
+import OpenFile (getOption)
+import Data.Maybe(fromMaybe)
+
+createDoc :: String -> String -> IO ()
+createDoc filecont output = if output /= "stdout"
+    then writeFile output filecont
+    else putStrLn filecont
+
+{-//TODO
+    get the string to
+-}
+
+writeTheDoc :: IO ()
+writeTheDoc = do
+    let app = getOption
+    app' <- app
+    createDoc (content app') (fromMaybe "" (oOutput (opt app')))
