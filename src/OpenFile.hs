bb605e4b4f8433aa99ddf89c09525275b691c755
6im0n <simon.gl3135@orange.fr>
2024-04-27 16:30:09 +0200

    ♻️ Refactor openFile function in OpenFile.hs to improve readability and error handling

--- src/OpenFile.hs
+++ src/OpenFile.hs
@@ -20,22 +20,17 @@ import Data.Maybe
 import GHC.IO.Exception
 import System.Exit
 
-openFile ::IO App
+openFile :: IO App
 openFile = do
-    let args =  parseArgs
+    let args = parseArgs
     app <- args
     let options = opt app
     let input = fromMaybe "" (oInput options)
-    if input == "" then
-        hPutStrLn stderr "Error: no input file" >>
-            putStrLn input >>
+    if input == ""
+        then hPutStrLn stderr "Error: no input file" >> putStrLn input >>
             exitWith (ExitFailure 84)
-    else do
-        file <- readFile input
-        return App {
-            opt = options,
-            content = file
-        }
+        else readFile input >>=
+            \file -> return App { opt = options, content = file }
 
 getFormat :: String -> String
 getFormat input
@@ -44,21 +39,17 @@ getFormat input
     | (x:_) <- input, x == '-' = "MD"
     | otherwise = "unknown"
 
+getOutput :: String -> String
+getOutput out = if out == "" then "stdout" else out
+
 getOption :: IO App
 getOption = do
     let app = openFile
     app' <- app
     let options = opt app'
     let format = getFormat (content app')
+    let output = getOutput (fromMaybe "" (oOutput options))
     return App {
-        opt = options { oIformat = Just format },
+        opt = options { oIformat = Just format, oOutput = Just output},
         content = content app'
     }
-
-printOptions :: App -> IO ()
-printOptions app = do
-    let options = opt app
-    putStrLn "Input Format: " >> putStrLn (fromMaybe "" (oIformat options))
-    putStrLn "Output Format: " >> putStrLn (fromMaybe "" (oOformat options))
-    putStrLn "Output Path: " >> putStrLn (fromMaybe "" (oOutput options))
-    putStrLn "Input Path: " >> putStrLn (fromMaybe "" (oInput options))
