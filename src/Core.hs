3e3993efc1082b8baa5e5ddb7f1dcc5aedd64038
6im0n <simon.gl3135@orange.fr>
2024-04-28 17:57:20 +0200

    :bug: Fix bug when the type of input is mardown

--- src/Core.hs
+++ src/Core.hs
@@ -30,10 +30,10 @@ createDoc filecont output = writeFile output filecont `catch`
     (`handleError` "Error: failed to write output file")
 
 getParserContent :: App -> Maybe Document
-getParserContent (App (Options (Just "json") _ _ _) content) =
-    parseJSON content
-getParserContent (App (Options (Just "xml") _ _ _) content) =
-    getXMLDocument content
+getParserContent (App (Options (Just "json") _ _ _) contentf) =
+    parseJSON contentf
+getParserContent (App (Options (Just "xml") _ _ _) contentf) =
+    getXMLDocument contentf
 getParserContent _ = Nothing
 
 writeDocString :: App -> Maybe Document -> Maybe String
@@ -43,11 +43,13 @@ writeDocString (App (Options _ (Just "json") _ _) _)
     (Just doc) = Just (documentToJSON doc)
 writeDocString (App (Options _ (Just "markdown") _ _) _)
     (Just doc) = Just (writeMarkdownDocument doc)
+writeDocString (App (Options (Just "markdown") (Just _) _ _) _) _ = Just ""
 writeDocString _ _ = Nothing
 
 writeTheDoc :: IO ()
 writeTheDoc = do
     app <- getOption
+    print (getParserContent app)
     case writeDocString app (getParserContent app) of
         Just str -> createDoc str (fromMaybe "stdout" (oOutput (opt app)))
         Nothing -> handleError
